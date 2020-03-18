453663f61777ecb357d1a883bf6a74acc1133753
umairx97 <umair.bajwa@hotmail.com>
2020-03-18 22:23:49 +0500

    Update Component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import React, { Component } from 'react'
 import axios from 'axios'
 
 class App extends Component {
-  async componentDidMount () {
+  state = {
+    message: ''
+  }
+  async componentDidMount() {
     const apiResponse = await axios.get('/.netlify/functions/app')
     console.log('FUNCTION IS RESPONDING', apiResponse)
+    this.setState({ message: apiResponse.data })
   }
 
-  render () {
+  render() {
+    const { message } = this.state
     return (
       <div>
-        Hello world
+        <h1>The message below is from the function invocation</h1>
+        <h2>message: {message ? message : "The function will not run locally"}</h2>
       </div>
     )
   }
