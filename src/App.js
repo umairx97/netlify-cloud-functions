837a6bb2b3b0fdfd82957a45b2804cde7e9e2677
umairx97 <umair.bajwa@hotmail.com>
2020-03-18 21:39:12 +0500

    testing netlify functions

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  async componentDidMount() {
+    const { data } = await axios.get('/.netlify/functions/app');
+    console.log(data)
+  }
+
+  render() {
+    return (
+      <div>
+        Hello world
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
